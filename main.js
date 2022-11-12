475c3f918a139e58328622833227e3cebd43f884
rafiguadalupe <83619619+rafiguadalupe@users.noreply.github.com>
2022-11-12 10:47:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ Webcam.set({
 });
 camera=document.getElementById("camera");
 Webcam.attach("#camera");
-
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="captured_image">';
+    });
+}
 
 
 
